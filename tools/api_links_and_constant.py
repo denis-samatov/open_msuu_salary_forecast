52eba3ddd051046b195d5bd22d844aea824d83fd
Denis Samatov <98321287+denis-samatov@users.noreply.github.com>
2024-08-12 14:26:36 +0300

    Update api_links_and_constant.py

--- tools/api_links_and_constant.py
+++ tools/api_links_and_constant.py
@@ -1,6 +1,13 @@
-SEED = 42
-USD_TO_RUB = 90
-EUR_TO_RUB = 100
-API_HH_AREAS = r'https://api.hh.ru/areas'
-API_HH_PROFESSIONAL_ROLES = r'https://api.hh.ru/professional_roles'
-API_HH_VACANCIES = r'https://api.hh.ru/vacancies'
+import streamlit as st
+
+# Accessing general configuration values
+SEED = st.secrets["general"]["SEED"]
+USD_TO_RUB = st.secrets["general"]["USD_TO_RUB"]
+EUR_TO_RUB = st.secrets["general"]["EUR_TO_RUB"]
+API_HH_AREAS = st.secrets["general"]["API_HH_AREAS"]
+API_HH_PROFESSIONAL_ROLES = st.secrets["general"]["API_HH_PROFESSIONAL_ROLES"]
+API_HH_VACANCIES = st.secrets["general"]["API_HH_VACANCIES"]
+
+# Accessing API keys and sensitive information
+API_HH_KEY = st.secrets["api_keys"]["API_HH_KEY"]
+EMPLOYER_ID = st.secrets["api_keys"]["EMPLOYER_ID"]
