8372e602b56b0af0b4ada6c47c1f22dbbc9e6131
Denis Samatov <98321287+denis-samatov@users.noreply.github.com>
2024-08-12 12:11:42 +0300

    Update api_links_and_constant.py

--- tools/api_links_and_constant.py
+++ tools/api_links_and_constant.py
@@ -4,5 +4,3 @@ EUR_TO_RUB = 100
 API_HH_AREAS = r'https://api.hh.ru/areas'
 API_HH_PROFESSIONAL_ROLES = r'https://api.hh.ru/professional_roles'
 API_HH_VACANCIES = r'https://api.hh.ru/vacancies'
-API_HH_KEY = r'USEROHR34L63TUN44M07KT3LI5G4F402M99MK551T9VIVLJRNS869LVP952PNR8V'
-EMPLOYER_ID = r'895945'
