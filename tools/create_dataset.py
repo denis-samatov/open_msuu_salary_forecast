999e0f95d98d74d8b3d456b9b8b3ef5a742031da
Denis Samatov <98321287+denis-samatov@users.noreply.github.com>
2024-08-22 12:17:49 +0300

    Update create_dataset.py

--- tools/create_dataset.py
+++ tools/create_dataset.py
@@ -31,6 +31,7 @@ def download_nltk_data(resource):
 
 download_nltk_data('tokenizers/punkt')
 download_nltk_data('corpora/stopwords')
+download_nltk_data('corpora/wordnet')
 
 # Set random seed for reproducibility
 np.random.seed(SEED)
